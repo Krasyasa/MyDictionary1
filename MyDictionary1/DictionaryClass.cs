651174b0f1b1fc17afa318285b1fce646b6b9365
caganparmaksiz162005@gmail.com <caganparmaksiz162005@gmail.com>
2021-01-23 14:40:03 +0300

    Add project files.

---
+++ MyDictionary1/DictionaryClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary1
+{
+    class DictionaryClass<TKey,TValue>
+    {
+        public TKey[] keys;
+        public TValue[] values;
+
+        public DictionaryClass()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeys = keys;
+            TValue[] tempValue = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValue[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+    }
+}
---
+++ MyDictionary1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DictionaryClass<string, int> dictionaryClass = new DictionaryClass<string, int> { };
+            dictionaryClass.Add("Ahmet", 16);
+            dictionaryClass.Add("Mehmet", 30);
+
+            for (int i = 0; i < dictionaryClass.keys.Length; i++)
+            {
+                Console.WriteLine(dictionaryClass.keys[i].ToString() + " : " + dictionaryClass.values[i].ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
